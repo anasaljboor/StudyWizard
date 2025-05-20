75f7e1800b860fcc282b5fc4a44cb36ab20f58a4
Ayham <battahayham1@gmail.com>
2025-05-20 22:41:25 +0300

    Add signup page and fix login page issues

--- app/src/main/java/com/example/studywizard/MainActivity.kt
+++ app/src/main/java/com/example/studywizard/MainActivity.kt
@@ -4,44 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.studywizard.Navigation.AppNavigation
+import com.example.studywizard.auth.AuthViewModel
 import com.example.studywizard.ui.theme.StudyWizardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val authViewModel: AuthViewModel by viewModels()
         setContent {
             StudyWizardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigation(modifier = Modifier.padding(innerPadding) , authViewModel = authViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudyWizardTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/studywizard/auth/firebaseAuth.kt
@@ -0,0 +1,5 @@
+package com.example.studywizard.auth
+
+class firebaseAuth {
+
+}
