b661a2682954418fa8e6b5f5562f13a72d8e47d7
Ayham <battahayham1@gmail.com>
2025-05-20 22:41:25 +0300

    Add signup page and fix login page issues

---
+++ app/src/main/java/com/example/studywizard/Navigation/AppNavigation.kt
@@ -0,0 +1,29 @@
+package com.example.studywizard.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.studywizard.auth.AuthViewModel
+import com.example.studywizard.HomePage.homePage
+import com.example.studywizard.auth.LoginPage
+import com.example.studywizard.auth.SignupPage
+
+@Composable
+
+fun AppNavigation(modifier: Modifier = Modifier , authViewModel: AuthViewModel){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login" , builder = {
+        composable("login") {
+            LoginPage(modifier , navController , authViewModel)
+        }
+        composable("signup") {
+            SignupPage(modifier , navController , authViewModel)
+        }
+        composable("home") {
+            homePage(modifier , navController , authViewModel)
+        }
+    })
+}
